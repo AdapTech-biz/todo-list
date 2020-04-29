729087d8ad8b42c67df620e3e685d4f8514a5b02
xyd93 <xyd93@outlook.com>
2020-04-29 08:44:21 -0400

    Created welcome.jsp for view resolver

--- src/main/java/xyd/programming/controller/DemoController.java
+++ src/main/java/xyd/programming/controller/DemoController.java
@@ -13,4 +13,10 @@ public class DemoController {
     public String hello() {
         return "hello"; //resolves a view without the ResponseBody annotation
     }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome() {
+        return "welcome";
+    }
 }
