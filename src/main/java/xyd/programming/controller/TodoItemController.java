538613bba6ce2209110fa0b88b1d92cb4e1ef171
xyd93 <xyd93@outlook.com>
2020-04-29 17:41:00 -0400

    Added TodoItemService to configure with the data source. Created form to display/add items.

--- src/main/java/xyd/programming/controller/TodoItemController.java
+++ src/main/java/xyd/programming/controller/TodoItemController.java
@@ -1,19 +1,37 @@
 package xyd.programming.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import xyd.programming.model.TodoData;
+import xyd.programming.model.TodoItem;
+import xyd.programming.service.TodoItemService;
+import xyd.programming.service.TodoItemServiceImpl;
+import xyd.programming.util.AttributeNames;
 import xyd.programming.util.Mappings;
 import xyd.programming.util.ViewNames;
 
+import java.time.LocalDate;
+
+@Slf4j
 @Controller
 public class TodoItemController {
+
+    private final TodoItemService todoItemService;
+
+    @Autowired
+    public TodoItemController(TodoItemService todoItemService) {
+        this.todoItemService = todoItemService;
+    }
 
     // == Model Attributes ==
     @ModelAttribute
     public TodoData todoData() {
-        return new TodoData();
+        return todoItemService.getData();
     }
 
     // == Mappings ==
@@ -21,5 +39,19 @@ public class TodoItemController {
     @GetMapping(Mappings.ITEMS)
     public String items() {
         return ViewNames.ITEMS_LIST;
+    }
+
+    @GetMapping(Mappings.ADD_ITEM)
+    public String addEditItem(Model model) {
+        TodoItem todoItem = new TodoItem(LocalDate.now(), "", "");
+        model.addAttribute(AttributeNames.TODO_ITEM, todoItem);
+        return ViewNames.ADD_ITEM;
+    }
+
+    @PostMapping(Mappings.ADD_ITEM)
+    public String processItem(@ModelAttribute(AttributeNames.TODO_ITEM) TodoItem todoItem) {
+        log.info("Todo item from form: {}", todoItem);
+        todoItemService.addItem(todoItem);
+        return "redirect:/" + Mappings.ITEMS;
     }
 }
---
+++ src/main/java/xyd/programming/util/AttributeNames.java
@@ -0,0 +1,12 @@
+package xyd.programming.util;
+
+public final class AttributeNames {
+
+    // == Constants ==
+    public static final String TODO_ITEM = "todoItem";
+
+
+    // == Constructor ==
+    private AttributeNames () {}
+
+}
