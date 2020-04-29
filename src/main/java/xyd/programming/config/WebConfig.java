095114d9348ee9150d8f24a1402157d6a86957c3
xyd93 <xyd93@outlook.com>
2020-04-29 07:32:50 -0400

    Init setup of servlet for root path

---
+++ src/main/java/xyd/programming/config/WebConfig.java
@@ -0,0 +1,11 @@
+package xyd.programming.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackages = "xyd.programming")
+public class WebConfig {
+}
